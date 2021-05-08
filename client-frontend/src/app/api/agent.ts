00f6ba164aa8e2673f91a4f2dc70c8078c6b799f
Lavinia Bleoca <lavinia.bleoca@gmail.com>
2021-05-08 18:27:01 +0200

    Axios CR work not UD

--- client-frontend/src/app/api/agent.ts
+++ client-frontend/src/app/api/agent.ts
@@ -21,7 +21,7 @@ const Joblistings = {
     list: () => requests.get<Joblisting[]>('/joblistings'),
     details: (id: string) => requests.get<Joblisting>('/joblistings/${id}'),
     create: (joblisting: Joblisting) => axios.post<void>('/joblistings', joblisting),
-    update: (joblisting: Joblisting) => axios.put<void>('/joblistings/${id}', joblisting),
+    update: (joblisting: Joblisting) => axios.put<void>('/joblistings/${joblisting.id}', joblisting),
     delete: (id: string) => axios.delete<void>('/joblistings/${id}')
 }
 
