921bb5d38cce86681511dd5b598783f6a733f95b
Lavinia Bleoca <lavinia.bleoca@gmail.com>
2021-05-10 01:20:33 +0200

    CRD fungerar U fixas med ny migration till string i db.Date

--- DataLayer/DataLayer/Migrations/20210509231913_FixarUiCRUD.Designer.cs
+++ DataLayer/DataLayer/Migrations/20210509231913_FixarUiCRUD.Designer.cs
@@ -4,14 +4,16 @@ using DataLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataLayer.Migrations
 {
     [DbContext(typeof(JobDbContext))]
-    partial class JobDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210509231913_FixarUiCRUD")]
+    partial class FixarUiCRUD
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Domain/Domain/Joblisting.cs
+++ Domain/Domain/Joblisting.cs
@@ -11,7 +11,7 @@ namespace Domain
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         public string Category { get; set; }
 
         public string City { get; set; }
